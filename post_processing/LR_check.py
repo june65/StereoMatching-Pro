9ebcae96a65864cb01628e579937e0465a5a455b
june65 <june0100726@gmail.com>
2024-08-08 22:40:38 +0900

    Fix left right consistency check

--- post_processing/LR_check.py
+++ post_processing/LR_check.py
@@ -3,11 +3,16 @@ import cv2
 from tqdm import tqdm
 def LR_check(left_disparity, right_disparity, depth):
     height, width = left_disparity.shape
-    aggregated_disparity = np.full((height, width), np.inf)
-    for w in range(width-depth):
+    disparity_mask = np.zeros((height, width))
+    for w in range(width):
         for h in range(height):
-            if left_disparity[h,w] == right_disparity[h,w+depth]:
-                aggregated_disparity[h,w] = left_disparity[h,w]
+            left_dis = left_disparity[h,w]
+            if w+left_dis < width:
+                right_dis = right_disparity[h,w+left_dis]
+                if right_dis == left_dis:
+                    disparity_mask[h,w] = 1
+
+    aggregated_disparity = left_disparity * disparity_mask
 
     print_img = aggregated_disparity.astype(np.uint8) * int(255 / depth)
     cv2.imshow('aggregated_disparity',print_img)
