Fix accuracy calculation

accuracy_calculation/Bad_ratio.py
@@ -5,7 +5,7 @@ def Bad_ratio(GT_image, test_image, depth, threshold):
         raise ValueError("Different Size")
     
     height, width = GT_image.shape
-    crop_GT_image = GT_image[depth:height-depth,depth:width-depth] * float(depth / 255)
+    crop_GT_image = GT_image[depth:height-depth,depth:width-depth] / float(16)
     crop_test_image = test_image[depth:height-depth,depth:width-depth]
     
     difference = np.abs(crop_GT_image - crop_test_image)